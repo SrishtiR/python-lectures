3f84fea4b0e4fc45d9b944624b1738cebfd0440e
srishtirawal <srishti.rawal2012@vit.ac.in>
2019-03-28 12:22:29 -0600

    added cube

--- maths.py
+++ maths.py
@@ -1,2 +1,5 @@
 def square(x):
     return x ** 2
+
+def cube(x):
+	return x ** 3
--- test_maths.py
+++ test_maths.py
@@ -1,4 +1,8 @@
-from maths import square
+from maths import square, cube
 def test_square():
     x = 4
     assert square(x) == 16
+
+def test_cube():
+    x = 4
+    assert cube(x) == 64
